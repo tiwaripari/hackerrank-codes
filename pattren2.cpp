6425b1bff921335e97dcab74e600826c1fea8792
tiwaripari <34934229+tiwaripari@users.noreply.github.com>
2018-02-23 21:13:42 +0530

    Add files via upload

---
+++ pattren2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std ;
+int main()
+{  int a,b;
+    for(int a=1;a<=5;a++)
+
+
+        {
+
+         for(int b=1;b<=a;b++)
+            cout<< " * ";
+            cout<<"\n";
+        }
+
+
+
+
+
+
+
+    return 0;
+}
