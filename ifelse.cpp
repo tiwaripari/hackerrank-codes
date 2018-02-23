e66ddb9e5e9a09a510e518d0b64c9bcbe4e5be2e
tiwaripari <34934229+tiwaripari@users.noreply.github.com>
2018-02-23 20:33:02 +0530

    Add files via upload

---
+++ ifelse.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+    if(a==1)
+        cout<<"      MONDAY   "<<endl;
+
+    else if(a==2)
+        cout<<"   TUESDAY  "<<endl;
+    else if(a==3)
+        cout<<"   WEDNESDAY   "<<endl;
+    else if(a==4)
+        cout<<"   THURSDAY    "<<endl;
+    else if(a==5)
+        cout<<"   FRIDAY     "<<endl;
+    else if(a==6)
+        cout<<"   SATURDAY   "<<endl
+    else if(a==7)
+        cout<<"    SUNDAY    "<<endl
+    return 0
+}
+
