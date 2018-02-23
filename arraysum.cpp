fd6ef3fd0498797ed11bbee641aedf8a4d59f55c
tiwaripari <34934229+tiwaripari@users.noreply.github.com>
2018-02-23 20:26:16 +0530

    Add files via upload

---
+++ arraysum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+ {
+     int arr[100]={0},i,n,sum=0;
+     cout<<"Enter the number of numbers you want"<<endl;
+     cin>>n;
+     cout<<"Enter "<<n<<"numbers"<<endl;
+
+     for(i=0;i<n;i++)
+        cin>>arr[i];
+
+     for(i=0;i<n;i++)
+        sum = arr[i];
+
+        cout<<sum;
+
+     return 0;
+ }
