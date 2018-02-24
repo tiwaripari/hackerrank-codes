d9723fc25fe36c493093bb829e697b41ec5d56ee
tiwaripari <34934229+tiwaripari@users.noreply.github.com>
2018-02-25 00:15:12 +0530

    Add files via upload

---
+++ pattern 3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    for(int a=1;a<=10;a++)
+    {
+        for(int b=a;b<=10;b++)
+
+              cout<<"            *           "<<endl;
+              cout<<"\n";
+
+
+    }
+
+    return 0;
+}
