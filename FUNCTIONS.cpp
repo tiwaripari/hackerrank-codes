cbd87fadcc8218b20e01a5d08e401cadf6094752
tiwaripari <34934229+tiwaripari@users.noreply.github.com>
2018-02-24 23:49:56 +0530

    Add files via upload

---
+++ FUNCTIONS.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int add(int x,int y)
+{
+    int c=x+y;
+    return c;
+}
+int main()
+{
+int a,b;
+cin>>a>>b;
+int sum=add(a,b);
+cout<<sum;
+return 0;
+}
+
+
+
+
