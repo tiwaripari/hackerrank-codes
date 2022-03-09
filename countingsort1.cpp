2dd59acae880a7155ac7fe35a4a73d6616eb0e6a
tiwaripari <34934229+tiwaripari@users.noreply.github.com>
2022-03-10 01:58:48 +0530

    Add files via upload

---
+++ countingsort1.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[1000000]={},brr[101]={};
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        int count=1;
+        if(brr[arr[i]]!=0)
+        {
+            continue;
+        }
+        else
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                 if(arr[i]==arr[j])
+                 {
+                     count++;
+                 }
+            }
+            brr[arr[i]]=count;
+        }
+    }
+    for(int i=0;i<100;i++)
+    {
+        cout<<brr[i]<<" ";
+    }
+    return 0;
+}
+
